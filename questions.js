c19e466ada97d656c411c408d82b5f68c5cf3cd2
Krishna sis Tripathy <108582463+siskrishna@users.noreply.github.com>
2023-01-08 19:11:38 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  // QUESTION 1
+  {
+    numb: 1,
+    question: "What was the name of third hokage in naruto ?",
+    answer: "Hiruzen Sarutobi",
+    options: [
+      "Minato Namikaze",
+      "Hiruzen Sarutobi",
+      "Tobirama Senju",
+      "Hashirama Senju"
+    ]
+  },
+
+  // QUESTION 2
+  {
+    numb: 2,
+    question: "Wwho is the writer of naruto series?",
+    answer: "Masashi Kishimoto",
+    options: [
+      "Eiichiro Oda",
+      "Hajime Isayama",
+      "Tsugumi Ohba",
+      "Masashi Kishimoto"
+    ]
+  },
+
+  // QUESTION 3
+  {
+    numb: 3,
+    question: "to which village sumaru a character of the naruto series belong to?",
+    answer: "Hidden Star Village",
+    options: [
+      "Hidden Star Village",
+      "Hidden Leaf Village",
+      "Hidden mist village",
+      "Hidden stone village"
+    ]
+  },
+
+  // QUESTION 4
+  {
+    numb: 4,
+    question: "on which genin a curse mark was left by orochimaru during the cunin exams?",
+    answer: "Sasuke Uchiha",
+    options: [
+      "Shikamaru Nara",
+      "Sakura Haruno",
+      "Naruto Uzumaki",
+      "Sasuke Uchiha"
+    ]
+  },
+
+  // QUESTION 5
+  {
+    numb: 5,
+    question: "who is the father of naruto uzumaki?",
+    answer: "Minato Namikaze",
+    options: [
+      "Minato Namikaze",
+      "Kazuto Uzumaki",
+      "Kushina Uzumaki",
+      "Ogata Shuma Hiroyuki"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
